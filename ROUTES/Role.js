27b1efdc0c371545bc8e686996758b81ef91ddbc
Eric-2804 <ericdayan13@gmail.com>
2025-07-21 15:47:42 -0500

    prueba

--- ROUTES/Role.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createRole, getRoles, deactivateRole } = require('../controllers/roles');
-
-router.post('/create', createRole);
-router.get('/', getRoles);
-router.patch('/deactivate/:id', deactivateRole);
-
-module.exports = router;
